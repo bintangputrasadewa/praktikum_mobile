33a83ef4dc4ca08bb52808e9ccdc1b7d3a8210ff
bintangputra <dicecool09@gmail.com>
2024-09-20 09:19:09 +0800

    update

--- p2/lib/main.dart
+++ p2/lib/main.dart
@@ -7,79 +7,66 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Praktikum2',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: true,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false, // Menyembunyikan banner debug
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 200,
-            height: 200,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadiusDirectional.all(
-                Radius.circular(10),
-              )
-              
-            ),
-          )
-        ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pertemuan 2"),
       ),
-    ),
-  );
-}
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Untuk mengatur agar kolom berada di tengah
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Mengatur jarak antar item di dalam Row
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  margin: const EdgeInsets.all(10), // Menggunakan nilai tetap untuk margin
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.green,
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
